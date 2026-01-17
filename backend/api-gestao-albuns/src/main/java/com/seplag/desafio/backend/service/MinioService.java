c712de543da0b7b196963e1e10ab886a91214d78
agneloobruno <brunoagnelo.almeida@gmail.com>
2026-01-17 00:23:54 -0400

    adiciona configuração do MinIO e implementa serviço para upload de arquivos

--- backend/api-gestao-albuns/src/main/java/com/seplag/desafio/backend/service/MinioService.java
+++ backend/api-gestao-albuns/src/main/java/com/seplag/desafio/backend/service/MinioService.java
@@ -1,2 +1,52 @@
-package com.seplag.desafio.backend.service;public class MinioService {
+package com.seplag.desafio.backend.service;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor // Lombok criando o construtor pra nós!
+public class MinioService {
+
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucket-name}")
+    private String bucketName;
+
+    public String upload(MultipartFile file) {
+        try {
+            // 1. Verificar se o bucket existe, se não, criar
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+
+            // 2. Gerar um nome único para o arquivo (para não sobrescrever)
+            String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+            // 3. Enviar pro MinIO
+            InputStream inputStream = file.getInputStream();
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(fileName)
+                            .stream(inputStream, file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build()
+            );
+
+            return fileName; // Retorna o nome salvo para guardarmos no banco
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fazer upload da imagem: " + e.getMessage());
+        }
+    }
 }
