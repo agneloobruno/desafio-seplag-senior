22757b3a1745d771dbbd0b312ebeccb18564a266
agneloobruno <brunoagnelo.almeida@gmail.com>
2026-01-21 13:48:25 -0400

    remove arquivo de configuração local do banco de dados, atualiza configurações do projeto e corrige parâmetros de conexão no application.yaml

--- backend/api-gestao-albuns/src/main/java/com/seplag/desafio/backend/infra/springdoc/SpringDocConfigurations.java
+++ backend/api-gestao-albuns/src/main/java/com/seplag/desafio/backend/infra/springdoc/SpringDocConfigurations.java
@@ -5,8 +5,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement; // IMPORTANTE
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer; // Importante
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,9 +17,7 @@ public class SpringDocConfigurations {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-
                 .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
-
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme()
@@ -34,5 +33,25 @@ public class SpringDocConfigurations {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://desafio.seplag/api/licenca")));
+    }
+
+    /*
+     * --- A CORREÇÃO DO ERRO DE SORT ---
+     * Este Bean ensina o Swagger a limpar os parametros de Sort/Pageable
+     * para que eles não sejam enviados como arrays bugados ["nome,asc"].
+     */
+    @Bean
+    public OpenApiCustomizer swaggerSortCustomizer() {
+        return openApi -> openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+            if (operation.getParameters() != null) {
+                operation.getParameters().forEach(parameter -> {
+                    if ("sort".equals(parameter.getName())) {
+                        // Força o Swagger a entender que 'sort' é uma coleção simples de strings
+                        // e não um array complexo que ele tenta serializar com colchetes
+                        parameter.getSchema().setType("string");
+                    }
+                });
+            }
+        }));
     }
 }
