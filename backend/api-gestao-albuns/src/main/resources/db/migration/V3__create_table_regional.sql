529b31e7e08b01a835b0bac6c638170f04324bed
agneloobruno <brunoagnelo.almeida@gmail.com>
2026-01-20 23:13:36 -0400

    remove tabelas usuario e regional da migração do banco de dados

--- backend/api-gestao-albuns/src/main/resources/db/migration/V3__create_table_regional.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE regional (
-                          id BIGSERIAL PRIMARY KEY,
-                          id_externo INTEGER NOT NULL,
-                          nome VARCHAR(200) NOT NULL,
-                          ativo BOOLEAN NOT NULL DEFAULT TRUE,
-                          data_sincronizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE INDEX idx_regional_id_externo ON regional(id_externo);
